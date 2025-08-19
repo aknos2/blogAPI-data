29510b827f8c3ff06a9525464b4e5e48ae569e27
Aknos <161412826+eltontomiyoshi@users.noreply.github.com>
2025-08-19 15:33:47 +0900

    add helmet and express-rate-limit. Fixed cors code security

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,24 +1,7 @@
 import prisma from "../lib/prisma.js";
-import bcrypt from 'bcryptjs';
 
 async function main() {
-  const DEFAULT_AVATAR = '/assets/corgi/profile/white-cat-icon.png';
-
-  const plainPassword = 'tomiyoshi3218';
-  const hashedPassword = await bcrypt.hash(plainPassword, 10);
-
-  await prisma.user.upsert({
-    where: { username: 'admin' },
-    update: {}, // keep empty if you don't want to change existing admin
-    create: {
-      username: 'admin',
-      password: hashedPassword,
-      role: 'ADMIN',
-      avatar: DEFAULT_AVATAR
-    }
-  });
-
-  console.log('✅ Admin user seeded successfully');
+  //somde code here
 }
 
 main()
